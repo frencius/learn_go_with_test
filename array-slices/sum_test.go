f21ee89afde92b7be7755c0b078d784f89023311
frencius <frencius@ruma.co.id>
2019-02-11 08:34:13 +0700

    change use of array to slices

--- array-slices/sum_test.go
+++ array-slices/sum_test.go
@@ -3,12 +3,15 @@ package array_slices
 import "testing"
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
 
-	got := Sum(numbers)
-	want := 15
+	t.Run("using array", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
 
-	if want != got {
-		t.Errorf("got %d, but want %d, given %v", got, want, numbers)
-	}
+		got := Sum(numbers)
+		want := 15
+
+		if want != got {
+			t.Errorf("got %d, but want %d, given %v", got, want, numbers)
+		}
+	})
 }
